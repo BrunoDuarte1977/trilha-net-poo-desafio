3bb07f5ed4d1357fccbdc7291c490c02a44d6bbb
Bruno Duarte <brunoduarte1977@gmail.com>
2024-09-25 17:17:41 -0300

    Desafio concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("4199988665544", "XPTO", "XYZ123", 512);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("jogo da cobrinha");
+
+Iphone iphone = new Iphone("4199988665544", "XPTO", "XYZ123", 512);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Clash Of Clans");
